8efc1d5a6ce181222e4091f67930b35092c69b02
Shivam Kushwaha <shivkush512@gmail.com>
2025-05-01 22:41:25 +0530

    Implemented config.js for secure API key management

---
+++ config.js
@@ -0,0 +1,32 @@
+// Firebase configuration keys
+const firebaseConfigs = {
+  dashboard: {
+    apiKey: "AIzaSyDYeUBOzm1LVxmgHOvkT1GBX4_hZPAIcZM",
+    authDomain: "testbin-586ac.firebaseapp.com",
+    databaseURL: "https://testbin-586ac-default-rtdb.firebaseio.com",
+    projectId: "testbin-586ac",
+    storageBucket: "testbin-586ac.appspot.com",
+    messagingSenderId: "1042607884233",
+    appId: "1:1042607884233:web:d4d1c3f0a8e5b9d4b5b8b8"
+  },
+  workers: {
+    apiKey: "AIzaSyDtKpcK59mijtVzn8j2ZpRyFMGzQE7b40U",
+    authDomain: "testbin-586ac.firebaseapp.com",
+    databaseURL: "https://testbin-586ac-default-rtdb.firebaseio.com",
+    projectId: "testbin-586ac",
+    storageBucket: "testbin-586ac.firebasestorage.app",
+    messagingSenderId: "395114814476",
+    appId: "1:395114814476:web:c7129570ab843bb98ea229",
+    measurementId: "G-Q8HWER1L33"
+  },
+  settings: {
+    apiKey: "AIzaSyDtKpcK59mijtVzn8j2ZpRyFMGzQE7b40U",
+    authDomain: "testbin-586ac.firebaseapp.com",
+    databaseURL: "https://testbin-586ac-default-rtdb.firebaseio.com",
+    projectId: "testbin-586ac",
+    storageBucket: "testbin-586ac.firebasestorage.app",
+    messagingSenderId: "395114814476",
+    appId: "1:395114814476:web:c7129570ab843bb98ea229",
+    measurementId: "G-Q8HWER1L33"
+  }
+};
